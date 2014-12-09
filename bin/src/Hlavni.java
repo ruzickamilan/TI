3a98b13b2bc1b63823fb7bb7eb009195184dac8c
ruzickamilan <milda.ruzicka@gmail.com>
2014-12-09 18:16:21 +0100

    Aplikace je ubalená ve spustitelném .jar souboru

---
+++ bin/src/Hlavni.java
@@ -0,0 +1,256 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
+import java.util.Timer;
+
+import javax.swing.*;
+
+/**
+ * 
+ * @author Milan Růžička
+ */
+public final class Hlavni extends JFrame {
+    private static final long serialVersionUID = 1L;
+    /** Timer pro dig. hodiny */
+    private final Timer timer1 = new Timer();
+    /** Timer pro stopky */
+    private Timer timer2;
+    /** Panel s časem/stopkama */
+    private Stopky stopky = new Stopky();
+    /** Počet kliknutí na tlačítko Mode */
+    private int pocet_kliku = 0;
+    /** Počet kliknutí na tlačítko Stop (pro vynulování stopek) */
+    private int vynulovani_stopek = 0;
+    /** Defaultní barva dig. hodin */
+    public static Color barva_hodin = Color.GREEN;
+    /** Defaultní barva stopek */
+    public static Color barva_stopek = Color.BLUE;
+    /** Barva měněných hodnot */
+    private final Color barva_hodnot = Color.RED;
+    
+    /**
+     * Konstruktor Hlavní třídy, nadefinování vzhledu okna a běží zde timer pro dig. hodiny
+     */
+    public Hlavni() {
+        this.setTitle("Stopky");
+        this.add(levaTlacitka(),BorderLayout.WEST);
+        this.add(pravaTlacitka(),BorderLayout.EAST);
+        this.add(stopky, BorderLayout.CENTER);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setMinimumSize(new Dimension(450, 110));
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+        stopky.setBackground(Color.BLACK);
+
+        timer1.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                        //Nastaví se čas o jednu sekundu větší
+                        // .getTime().getTime() je tu proto aby mi to vrátilo milisekundy
+                        // jenom .getTime() vrátí čas ve formátu Time (můj getter ze třídy Stopky)
+                        stopky.setTime(new Time(stopky.getTime().getTime()+1000));
+                        //aby se čas překresoval tam kde má (lze poznat dle konzole že to funguje)
+                        if (stopky.getMode() == 0)
+                                stopky.repaint();
+                }
+        }, 1000, 1000);	
+    }
+
+    /**
+     * Levý panel se tlačítkem Mode
+     * 
+     * @return Panel s tlačítkem Mode
+     */
+    final Component levaTlacitka() {
+        JPanel tlacitka = new JPanel();
+        tlacitka.setPreferredSize(new Dimension(80, 50));
+        tlacitka.setBackground(Color.BLACK);
+
+        JButton mode = new JButton(new Mode());
+        mode.setPreferredSize(new Dimension(70, 30));
+        tlacitka.add(mode);
+
+        return tlacitka;
+    }
+
+    /**
+     * Pravý panel s tlačítky Start a Stop
+     * 
+     * @return Panel s tlačítky Start a Stop
+     */
+    Component pravaTlacitka() {
+        JPanel tlacitka = new JPanel();
+        tlacitka.setPreferredSize(new Dimension(80, 50));
+        tlacitka.setBackground(Color.BLACK);
+
+        JButton start = new JButton(new Start());
+        start.setPreferredSize(new Dimension(70, 30));
+        JButton stop = new JButton(new Stop());
+        stop.setPreferredSize(new Dimension(70, 30));
+        tlacitka.add(start);
+        tlacitka.add(stop);
+
+        return tlacitka;
+    }
+
+    /**
+     * Slouží k přepínání mezi stavy aplikace (mění se barvičky, pocet_kliku -> pro tlačítka Start/Stop)
+     * 
+     * @author Milan Růžička
+     */
+    class Mode extends AbstractAction {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            switch (pocet_kliku) {
+                case 0: 
+                    stopky.setBarva(barva_hodnot); 
+                    stopky.setIndex(2); 
+                    stopky.repaint();
+                    pocet_kliku++;
+                    break;
+                case 1: 
+                    stopky.setBarva(barva_hodnot); 
+                    stopky.setIndex(1); 
+                    stopky.repaint(); 
+                    pocet_kliku++;
+                    break;
+                case 2: 
+                    stopky.setBarva(barva_hodnot); 
+                    stopky.setIndex(0); 
+                    stopky.repaint(); 
+                    pocet_kliku++;
+                    break;
+                case 3: 
+                    stopky.setMode(1);
+                    stopky.setIndex(3);
+                    stopky.repaint(); 
+                    pocet_kliku++;
+                    break;
+                default: 
+                    stopky.setMode(0);
+                    stopky.setBarva(barva_hodin); 
+                    stopky.setIndex(0); 
+                    stopky.repaint(); 
+                    pocet_kliku = 0;
+                    break;
+            }
+        }
+
+        public Mode() {
+            putValue(NAME, "Mode");
+            putValue(SHORT_DESCRIPTION, "Přepnutí režimu stopek (ALT+M)");
+            putValue(MNEMONIC_KEY, KeyEvent.VK_M);
+        }
+    }
+
+    /**
+     * Zde se přidávají sekundy/minuty/hodiny dle pocet_kliku na Mode a spouští se zde stopky
+     * 
+     * @author Milan Růžička
+     */
+    class Start extends AbstractAction {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            switch (pocet_kliku) {
+                case 1:
+                    stopky.setTime(new Time(stopky.getTime().getTime()+1000));
+                    stopky.repaint();
+                    break;
+                case 2:
+                    stopky.setTime(new Time(stopky.getTime().getTime()+(1000*60)));
+                    stopky.repaint();
+                    break;
+                case 3:
+                    stopky.setTime(new Time(stopky.getTime().getTime()+(1000*60*60)));
+                    stopky.repaint();
+                    break;
+                case 4:
+                    if (vynulovani_stopek == 0) {
+                        vynulovani_stopek = 1;
+                        timer2 = new Timer();
+                        timer2.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                stopky.setStopky(new Time(stopky.getStopky().getTime()+1000));
+                                if (stopky.getMode() == 1)
+                                    stopky.repaint();
+                            }
+                        }, 1000, 1000);
+                    }
+                default: break;
+            }
+        }
+
+        public Start() {
+            putValue(NAME, "Start");
+            putValue(SHORT_DESCRIPTION, "Změna hodnoty o +1, nebo spuštění stopek (ALT+A)");
+            putValue(MNEMONIC_KEY, KeyEvent.VK_A);
+        }
+    }
+
+    /**
+     * Zde se odebírají sekundy/minuty/hodiny dle pocet_kliku na Mode a zastavují/nulují se zde stopky
+     * 
+     * @author Milan Růžička
+     */
+    class Stop extends AbstractAction {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            switch (pocet_kliku) {
+                case 1:
+                    stopky.setTime(new Time(stopky.getTime().getTime()-1000));
+                    stopky.repaint();
+                    break;
+                case 2:
+                    stopky.setTime(new Time(stopky.getTime().getTime()-(1000*60)));
+                    stopky.repaint();
+                    break;
+                case 3:
+                    stopky.setTime(new Time(stopky.getTime().getTime()-(1000*60*60)));
+                    stopky.repaint();
+                    break;
+                case 4:
+                    if (vynulovani_stopek == 1) {
+                            timer2.cancel();
+                            vynulovani_stopek = 0;
+                    }
+                    else if (vynulovani_stopek == 0) {
+                            stopky.setStopky(new Time(-3600000));
+                            stopky.repaint();
+                    }
+                default: break;
+            }
+        }
+
+        public Stop() {
+            putValue(NAME, "Stop");
+            putValue(SHORT_DESCRIPTION, "Změna hodnoty o -1, nebo zastavení stopek (ALT+S)");
+            putValue(MNEMONIC_KEY, KeyEvent.VK_S);
+        }
+    }
+
+    /**
+     *
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        Hlavni hlavni = new Hlavni();
+    }
+}
---
+++ bin/src/SegmentoveCislo.java
@@ -0,0 +1,189 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.*;
+
+/**
+ * Třída SegmentoveCislo přijme do konstruktoru cas ve stringu, grafický kontext a barvu/index dané části
+ * módu dig. hodinek a vykreslí na předaný grafický kontext čas v sedmisegmentovém tvaru
+ * 
+ * @author Milan Růžička
+ */
+public final class SegmentoveCislo {
+    /** Čas, který se bude vykreslovat */
+    private final String cas;
+    /** Barva písma dig. hodin */
+    private Color barva_pisma;
+
+    /**
+     * Základní konstruktor této třídy, rozseká čas na jednotlivé pozice(hh:mm:ss) a zavolá si metodu 
+     * vykresli pro vykreslení dané části času
+     * 
+     * @param cas Celý čas ve stringu
+     * @param g2 Grafický kontext
+     * @param barva Barva části času (se kterou se manipuluje pomocí Start/Stop)
+     * @param index Index části času (se kterou se manipuluje pomocí Start/Stop)
+     */
+    public SegmentoveCislo(String cas, Graphics2D g2, Color barva, int index) {
+        this.cas = cas;
+        String[] pole = this.cas.split(":");
+        for (int i = 0; i < pole.length; i++) {
+            int cislo = Integer.parseInt(pole[i]);
+            vykresli(g2, cislo, i, barva, index);
+        }
+    }
+
+    /**
+     * Metoda která vykresluje postupně jednotlivé číslice času. Jsou zde nadefinovány všechny souřadnice
+     * dílků sedmisegmentového písma a dle číslice se dílky vykreslují
+     * 
+     * @param g2 Grafický kontext
+     * @param cislo Číslo které cheme vykreslit
+     * @param posun Posun o který se posune číslo doprava (aby se čísla nepřekrývala a tvořila opravdu čas)
+     */
+    public void vykresliSegmentoveCislo(Graphics2D g2, int cislo, int posun) {
+        int[] dilek1X = {10+posun,15+posun,30+posun,35+posun,30+posun,15+posun};
+        int[] dilek1Y = {10,5,5,10,15,15};
+        int[] dilek2X = {9+posun,4+posun,4+posun,9+posun,14+posun,14+posun};
+        int[] dilek2Y = {11,16,31,36,31,16};
+        int[] dilek3X = {36+posun,31+posun,31+posun,36+posun,41+posun,41+posun};
+        int[] dilek3Y = {11,16,31,36,31,16};
+        int[] dilek4X = {10+posun,15+posun,30+posun,35+posun,30+posun,15+posun};
+        int[] dilek4Y = {37,32,32,37,42,42};
+        int[] dilek5X = {9+posun,4+posun,4+posun,9+posun,14+posun,14+posun};
+        int[] dilek5Y = {38,43,58,63,58,43};
+        int[] dilek6X = {36+posun,31+posun,31+posun,36+posun,41+posun,41+posun};
+        int[] dilek6Y = {38,43,58,63,58,43};
+        int[] dilek7X = {10+posun,15+posun,30+posun,35+posun,30+posun,15+posun};
+        int[] dilek7Y = {64,59,59,64,69,69};
+
+        if (cislo == 0) {
+            g2.fillPolygon(dilek1X, dilek1Y, 6);
+            g2.fillPolygon(dilek2X, dilek2Y, 6);
+            g2.fillPolygon(dilek3X, dilek3Y, 6);
+            g2.fillPolygon(dilek5X, dilek5Y, 6);
+            g2.fillPolygon(dilek6X, dilek6Y, 6);
+            g2.fillPolygon(dilek7X, dilek7Y, 6);
+        }
+        if (cislo == 1) {
+            g2.fillPolygon(dilek3X, dilek3Y, 6);
+            g2.fillPolygon(dilek6X, dilek6Y, 6);
+        }
+        if (cislo == 2) {
+            g2.fillPolygon(dilek1X, dilek1Y, 6);
+            g2.fillPolygon(dilek3X, dilek3Y, 6);
+            g2.fillPolygon(dilek4X, dilek4Y, 6);
+            g2.fillPolygon(dilek5X, dilek5Y, 6);
+            g2.fillPolygon(dilek7X, dilek7Y, 6);
+        }
+        if (cislo == 3) {
+            g2.fillPolygon(dilek1X, dilek1Y, 6);
+            g2.fillPolygon(dilek3X, dilek3Y, 6);
+            g2.fillPolygon(dilek4X, dilek4Y, 6);
+            g2.fillPolygon(dilek6X, dilek6Y, 6);
+            g2.fillPolygon(dilek7X, dilek7Y, 6);
+        }
+        if (cislo == 4) {
+            g2.fillPolygon(dilek2X, dilek2Y, 6);
+            g2.fillPolygon(dilek3X, dilek3Y, 6);
+            g2.fillPolygon(dilek4X, dilek4Y, 6);
+            g2.fillPolygon(dilek6X, dilek6Y, 6);
+        }
+        if (cislo == 5) {
+            g2.fillPolygon(dilek1X, dilek1Y, 6);
+            g2.fillPolygon(dilek2X, dilek2Y, 6);
+            g2.fillPolygon(dilek4X, dilek4Y, 6);
+            g2.fillPolygon(dilek6X, dilek6Y, 6);
+            g2.fillPolygon(dilek7X, dilek7Y, 6);
+        }
+        if (cislo == 6) {
+            g2.fillPolygon(dilek1X, dilek1Y, 6);
+            g2.fillPolygon(dilek2X, dilek2Y, 6);
+            g2.fillPolygon(dilek4X, dilek4Y, 6);
+            g2.fillPolygon(dilek5X, dilek5Y, 6);
+            g2.fillPolygon(dilek6X, dilek6Y, 6);
+            g2.fillPolygon(dilek7X, dilek7Y, 6);
+        }
+        if (cislo == 7) {
+            g2.fillPolygon(dilek1X, dilek1Y, 6);
+            g2.fillPolygon(dilek3X, dilek3Y, 6);
+            g2.fillPolygon(dilek6X, dilek6Y, 6);
+        }
+        if (cislo == 8) {
+            g2.fillPolygon(dilek1X, dilek1Y, 6);
+            g2.fillPolygon(dilek2X, dilek2Y, 6);
+            g2.fillPolygon(dilek3X, dilek3Y, 6);
+            g2.fillPolygon(dilek4X, dilek4Y, 6);
+            g2.fillPolygon(dilek5X, dilek5Y, 6);
+            g2.fillPolygon(dilek6X, dilek6Y, 6);
+            g2.fillPolygon(dilek7X, dilek7Y, 6);
+        }
+        if (cislo == 9) {
+            g2.fillPolygon(dilek1X, dilek1Y, 6);
+            g2.fillPolygon(dilek2X, dilek2Y, 6);
+            g2.fillPolygon(dilek3X, dilek3Y, 6);
+            g2.fillPolygon(dilek4X, dilek4Y, 6);
+            g2.fillPolygon(dilek6X, dilek6Y, 6);
+            g2.fillPolygon(dilek7X, dilek7Y, 6);
+        }
+    }
+
+    /**
+     * Metoda která si volá o vykreslení jednotlivých číslic metodě vykresliSegmentovaciCislo
+     * 
+     * @param g2 Grafický kontext
+     * @param cislo Požadované dvoučíslí z času které chceme vykreslit
+     * @param pozice Pozice na které chceme dvoučíslí vykreslit (hh:mm:ss) -> (0:1:2)
+     * @param barva Barva části času (se kterou se manipuluje pomocí Start/Stop)
+     * @param index Index části času (se kterou se manipuluje pomocí Start/Stop)
+     */
+    public void vykresli(Graphics2D g2, int cislo, int pozice, Color barva, int index) {
+        if (index == 3)
+            barva_pisma = Hlavni.barva_stopek;
+        else
+            barva_pisma = Hlavni.barva_hodin;
+
+        g2.setColor(barva_pisma);
+
+        String cislo_string = "" + cislo;
+        int prvni_cislo = Integer.parseInt(cislo_string.substring(0, 1));
+        int druhe_cislo;
+        try {
+            druhe_cislo = Integer.parseInt(cislo_string.substring(1, 2));
+        }
+        catch (Exception e) {
+            prvni_cislo = 0;
+            druhe_cislo = Integer.parseInt(cislo_string.substring(0, 1));
+        }
+
+        if (pozice == 0) {
+            if (index == 0)
+                g2.setColor(barva);
+            vykresliSegmentoveCislo(g2, prvni_cislo, 0);
+            vykresliSegmentoveCislo(g2, druhe_cislo, 40);
+            g2.setColor(barva_pisma);
+            g2.fillOval(85, 22, 10, 10);
+            g2.fillOval(85, 42, 10, 10);
+        }
+        else if (pozice == 1) {
+            if (index == 1)
+                g2.setColor(barva);
+            vykresliSegmentoveCislo(g2, prvni_cislo, 95);
+            vykresliSegmentoveCislo(g2, druhe_cislo, 135);
+            g2.setColor(barva_pisma);
+            g2.fillOval(180, 22, 10, 10);
+            g2.fillOval(180, 42, 10, 10);
+        }
+        else if (pozice == 2) {
+            if (index == 2)
+                g2.setColor(barva);
+            vykresliSegmentoveCislo(g2, prvni_cislo, 190);
+            vykresliSegmentoveCislo(g2, druhe_cislo, 230);
+            g2.setColor(barva_pisma);
+        }
+    }
+}
